39c27d6ed16fc57c3a4a2f3adf40749aa63076cf
yanealdik <airm2@yanealdik-2.local>
2025-04-04 14:40:12 +0500

    Fix Fast Refresh warning by moving MainApp to separate file

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import Load from "./components/Loading/Load";
+import MainApp from "./MainApp.jsx";
 import "./App.css";
-
-function MainApp() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleStartLearning = () => {
-    setIsLoading(false);
-  };
-
-  return (
-    <>
-      {isLoading ? (
-        <Load onStartLearning={handleStartLearning} />
-      ) : (
-        <App />
-      )}
-    </>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
