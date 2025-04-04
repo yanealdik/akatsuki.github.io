6f8b6cb1d6265c56f7e7c457b62cadb67c4cf426
yanealdik <airm2@yanealdik-2.local>
2025-04-04 14:43:56 +0500

    Move MainApp to separate file to fix build issues

---
+++ src/MainApp.jsx
@@ -0,0 +1,22 @@
+
+import React, { useState } from "react";
+import App from "./App.jsx";
+import Load from "./components/Loading/Load";
+
+export default function MainApp() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleStartLearning = () => {
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <Load onStartLearning={handleStartLearning} />
+      ) : (
+        <App />
+      )}
+    </>
+  );
+}
