79fd986a616d7926a9d1615ebd78ca7342a51cab
yanealdik <airm2@yanealdik-2.local>
2025-04-04 14:59:21 +0500

    Обновил содержимое index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+    },
+  },
+});
