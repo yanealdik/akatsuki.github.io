26cacb598a0740aed01a6da2ce4a7c0dbb77f58b
yanealdik <airm2@yanealdik-2.local>
2025-04-21 16:40:14 +0500

    Add GitHub Actions workflow for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   base: '/akatsuki.github.io/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true, // поддержка "node:crypto"
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
@@ -15,7 +21,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@components': '/src/components',
-      // Убери crypto, если он не используется
     },
   },
   server: {
