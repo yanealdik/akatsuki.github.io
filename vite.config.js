5062ebfe706678a75dd7c4fd0fe21e6f5dbf725e
yanealdik <airm2@yanealdik-2.local>
2025-04-21 14:33:27 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/akatsuki.github.io/',
   resolve: {
     alias: {
       '@components': '/src/components',
@@ -13,4 +14,5 @@ export default defineConfig({
       '/api': 'http://127.0.0.1:5000',
     },
   },
+
 });
